b3488f0920dbede9711aa0554b2781c96c23e1ef
(hyungkey) <(hyungkey0601@gmail.com)>
2025-07-17 10:06:34 +0900

    init

---
+++ src/main/java/com/practice/member/user/domain/User.java
@@ -0,0 +1,11 @@
+package com.practice.member.user.domain;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter @NoArgsConstructor @AllArgsConstructor
+public class User {
+}
